1ebd43614138a5421802483338f2b61b2cea8642
iagomoraisn <iagojkd96@hotmail.com>
2023-08-04 15:47:00 -0300

    finalizando

--- src/index.ts
+++ src/index.ts
@@ -36,12 +36,15 @@ function gameLoop(
     view: CanvasView,
     bricks: Brick[],
     paddle: Paddle,
-    //ball: Ball
+    ball: Ball
 ) {
     console.log('draw');
     view.clear();
     view.drawBricks(bricks);
     view.drawSprite(paddle);
+    view.drawSprite(ball);
+    // move ball
+    ball.moveBall();
 
 
     // move paddle and check so it won't exit the playfield
@@ -53,7 +56,7 @@ function gameLoop(
         paddle.movePaddle();
     }
 
-    requestAnimationFrame(() => gameLoop(view, bricks, paddle));
+    requestAnimationFrame(() => gameLoop(view, bricks, paddle, ball));
 }
 
 function startGame(view: CanvasView) {
@@ -63,6 +66,13 @@ function startGame(view: CanvasView) {
     view.drawScore(0);
     // create all bricks
     const bricks = createBricks();
+    // create a ball
+    const ball = new Ball(
+        BALL_SPEED,
+        BALL_SIZE,
+        { x: BALL_STARTX, y: BALL_STARTY },
+        BALL_IMAGE
+    );
     // create a paddle
     const paddle = new Paddle(
         PADDLE_SPEED,
@@ -75,7 +85,7 @@ function startGame(view: CanvasView) {
         PADDLE_IMAGE
         );
 
-    gameLoop(view, bricks, paddle);
+    gameLoop(view, bricks, paddle, ball);
 }
 
 // create a new view
